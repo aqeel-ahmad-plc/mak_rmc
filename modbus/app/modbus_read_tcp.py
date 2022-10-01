7f9805b20e9cd8225a147f692caa0210c897a774
ahmad <ahmaduet0992@gmail.com>
2022-10-01 14:19:58 +0500

    modbus_tcp service reads all data at once

--- modbus/app/modbus_read_tcp.py
+++ modbus/app/modbus_read_tcp.py
@@ -8,20 +8,20 @@ while 1:
     client = connectModbusTCP("10.10.100.254", 8899, 1)
     if(client != None):
         x={}
-        data1 = readHolding(client, 1, 10)
-        data2 = readHolding(client, 11, 10)
-        Merge(data1, data2)
-        data1 = readHolding(client, 21, 10)
-        Merge(data1, data2)
-        data1 = readHolding(client, 31, 10)
-        Merge(data1, data2)
-        data1 = readHolding(client, 41, 10)
-        Merge(data1, data2)
-        data1 = readHolding(client, 51, 10)
-        Merge(data1, data2)
+        # data1 = readHolding(client, 1, 10)
+        # data2 = readHolding(client, 11, 10)
+        # Merge(data1, data2)
+        # data1 = readHolding(client, 21, 10)
+        # Merge(data1, data2)
+        # data1 = readHolding(client, 31, 10)
+        # Merge(data1, data2)
+        # data1 = readHolding(client, 41, 10)
+        # Merge(data1, data2)
+        # data1 = readHolding(client, 51, 10)
+        # Merge(data1, data2)
 
         # data2 = readHolding(client, 1, 100)
-
+        data2 = readHolding(client, 1, 100)
         print(data2)
         result = parseData(data2, 'config/energy_meter/parameters_config.json')
         for param in result:
@@ -35,5 +35,5 @@ while 1:
         else:
             print("Error occurred in saving to db")
 
-    time.sleep(0.5)
+    time.sleep(1)
 client.close()
