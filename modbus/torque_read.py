06977d646f7a47bb8368525138e1f0212e459711
ahmad <ahmaduet0992@gmail.com>
2022-12-28 13:01:15 +0500

    updated torque sensor code

--- modbus/torque_read.py
+++ modbus/torque_read.py
@@ -1,45 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import time
 import serial
 import requests
 import json
 import struct
 url = 'http://localhost/mak_rmc/torque/create'
+
 # configure the serial connections (the parameters differs on the device you are connecting to)
+
 ser = serial.Serial(
-	port='COM9',
-	baudrate=9600,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS,
-	timeout=2
-)
+    port='COM9',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=2,
+    )
 ser.close()
 
 
-while 1 :
+def ReadData():
     ser.open()
-    x = {}
-    #ser.write(bytes(b'05010000000006AA'))
-    cw = [0x23,0x30,0x30,0x0d]
+    cw = [0x23, 0x30, 0x30, 0x0d]
     ser.write(serial.to_bytes(cw))
-    print("write command")
-    print("reading the data")
     time.sleep(0.001)
-    print("After delay")
     data = ser.read(20)
     if len(data) == 0:
-        print("data is none")
-        time.sleep(5)
         ser.close()
-        continue
-    print((data.decode("utf-8")))
-    torque = float(data[1:8]) * 200;
-    print(torque)
-    #
-    x['torque'] = torque
+        return -1
+    ser.close()
+    torque = float(data[1:8]) * 200
+    return torque
+
+
+while 1:
+    x = {}
+
+    # print((data.decode("utf-8")))
+    average_torque = 0
+    start_time = time.time()
+    for i in range(0,10):
+        torque = ReadData()
+        average_torque = average_torque + torque
+        time.sleep(0.05)
+    average_torque = average_torque/10
+    x['torque'] = average_torque
+    print('--- %s seconds ---' % (time.time() - start_time))
     print(x)
-    # for param in result:
-    #     x[param['col_name']] = param['value']
+
     ret = requests.post(url, data = x)
     result = json.loads(ret.text)
     print(result)
@@ -47,14 +56,3 @@ while 1 :
         print("Data saved successfully")
     else:
         print("Error occurred in saving to db")
-    #
-    #     # for line in ser.read():
-    #     #     print("line", line)
-    # # except:
-    # #     print("Keyboard Interrupt")
-    #     break
-    ser.close()
-    time.sleep(0.002)
-
-
-    print("====================================")
